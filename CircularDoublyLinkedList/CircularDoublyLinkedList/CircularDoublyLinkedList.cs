6c2b561ad3cc5a88ee31ce59a7e553558cc3a4f0
AlecGMain <geosimonianalec@gmail.com>
2018-05-31 17:28:33 -0700

    Just beginning, setting up Node & CircularDoublyLinked List classes.

---
+++ CircularDoublyLinkedList/CircularDoublyLinkedList/CircularDoublyLinkedList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularDoublyLinkedList
+{
+    class CircularDoublyLinkedList<T>
+    {
+        public Node<T> Head;
+        public int Count;
+        public CircularDoublyLinkedList()
+        {
+            Head = new Node<T>();
+        }
+        public CircularDoublyLinkedList(T value)
+        {
+            Head = new Node<T>(value);
+        }
+    }
+}
